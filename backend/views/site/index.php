47adbfbd199cbb3d3d4ff51f22ad6b0c7bdb52e7
Nacho2126 <ignaciogp26@gmail.com>
2016-12-06 15:30:52 -0300

    Cambio Backend

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -4,9 +4,21 @@
 
 $this->title = 'My Yii Application';
 ?>
+
+<!-- background-image: url("/../images.jpg"); -->
+
+<body >
+
 <div class="site-index">
 
-    <div class="jumbotron">
+<br>
+<br>
+
+    <div class="alert alert-success">
+  <strong>Felicidades!</strong> Felicitaciones Ud se ha registrado Correctamente.
+</div>
+
+    <div background-color: "black"; class="jumbotron";>
         <h1>Bienvenido a QuieroCasa.com.uy</h1>
 
         <p class="lead">Taller de PHP 2016</p>
@@ -17,9 +29,29 @@ $this->title = 'My Yii Application';
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-lg-4"> 
+            <h3>Registrar un Inmueble</h3>
+                <a href="http://localhost:8080/php20162/backend/web/index.php?r=inmuebles%2Findex">Haz click <strong >AQUI</strong></a> 
 
+             </div>
+            <div class="col-lg-4">  
+                <h3>Registrar un Tipo de Inmueble</h3>
+                <a href="http://localhost:8080/php20162/backend/web/index.php?r=tipoinmueble%2Findex">Haz click <strong>AQUI</strong></a> 
+
+            </div>
+             <div class="col-lg-4">  
+                <h3>Registrar un Cliente</h3>
+                <a href="http://localhost:8080/php20162/backend/web/index.php?r=user%2Fadmin%2Findex">Haz click <strong>AQUI</strong></a> 
+
+            </div>
+           
+            
+        </div>
+
+
+     <!--   <div class="row">
+            <div class="col-lg-4">
+                
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
@@ -48,6 +80,7 @@ $this->title = 'My Yii Application';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
         </div>
-
+        -->
     </div>
 </div>
+</body>
